9ade0e30454c0ea94e43f41ffa4048ae64f3d2bb
jakub <jakub@DESKTOP-JHC3G20>
2023-10-26 14:29:21 +0200

    Add project files.

---
+++ School work/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace School_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge will du hyra bilen? Skriv i antal timmar.");
+            string timmar = Console.ReadLine();
+            int antl_timmar = int.Parse(timmar);
+
+            int resultat = antl_timmar * 80;
+
+            if (resultat >= 950)
+            {
+                Console.WriteLine("Kostand för " + timmar + " timmar är 950kr.");
+            }
+            else
+            {
+                Console.WriteLine("Konstand för " + timmar + " timmar är " + resultat);
+            }
+        }
+    }
+}
